432f7980a6799be47e42d1d1300b4a8cbd9fa832
neil314159 <neil@neilboland.com>
2021-11-24 11:26:21 +0000

    Add functionality to change game piece colour on click

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,1 +1,15 @@
- 
+document.addEventListener("DOMContentLoaded", function(){
+    let slots = document.getElementsByClassName("slot");
+
+
+    for(let slot of slots){
+        slot.addEventListener("click", function(){
+
+        
+        this.classList.add("green");
+    });
+    }
+
+    
+    
+})
