1dbe1c930d7c0b1f28e07963bfb6782e61429090
neil314159 <neil@neilboland.com>
2021-11-24 17:16:52 +0000

    Alter animation timing in CSS

--- assets/js/style.js
+++ assets/js/style.js
@@ -14,10 +14,12 @@ document.addEventListener("DOMContentLoaded", function(){
     let buttons = document.getElementsByClassName("menu");
     // console.log(buttons);
     buttons[0].addEventListener("click", function(){
-// alert("here");
-        // console.log("getting here");
+
         let faces = document.getElementsByClassName("box");
         faces[0].classList.add("spinback");
+        let shadows = document.getElementsByClassName("shadow");
+        console.log(shadows);
+        shadows[0].classList.add("spinbackshadow");
     });
 
     let buttons2 = document.getElementsByClassName("front");
@@ -28,6 +30,12 @@ document.addEventListener("DOMContentLoaded", function(){
         let faces = document.getElementsByClassName("box");
         faces[0].classList.remove("spinback");
         faces[0].classList.add("spinfront");
+
+
+        let shadows = document.getElementsByClassName("shadow");
+        console.log(shadows);
+        shadows[0].classList.remove("spinbackshadow");
+        shadows[0].classList.add("spinfrontshadow");
     });
 
     
