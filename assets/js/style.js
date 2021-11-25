13a498849c998d865ff804cd9bdf31483acf8551
neil314159 <neil@neilboland.com>
2021-11-25 01:34:01 +0000

    Add algorithm to count empty spaces on board

--- assets/js/style.js
+++ assets/js/style.js
@@ -47,10 +47,10 @@ function createBoard(){
          slot.setAttribute('data-id', i);
         slot.classList.add('slot');
         slot.classList.add('clear');
-        slot.addEventListener("click", function(){
-            //console.log(this);
-            this.classList.add("green");
-        });
+        slot.addEventListener("click", function(){gameClick(i)});
+        //     //console.log(this);
+        //     this.classList.add("green");
+        // });
         document.getElementById('game').appendChild(slot);
     }
 }
@@ -67,10 +67,38 @@ function clearBoard(){
     
 }
 
-function gameClick(){
-   
-       
+function gameClick(i){
+    let column = (i%7);
+    let empties = emptySpacesColumn(column);
+   //console.log(empties);
+
+    //let emptyspaces = emptySpacesColumn(i);
+    
+    let slots = document.getElementsByClassName("slot");
+     
+if(empties>0){
+    slot[].classList.add("green");
+}
+
+    //slots[i].classList.remove("clear"); 
+    //slots[i].classList.add("green");
     
 }
 
+function emptySpacesColumn(toCheck){
+    //console.log(toCheck);
+    let slots = document.getElementsByClassName("slot");
+    let emptyCounter = 0;
+
+     for(let i=toCheck; i<42; i+=7){
+        //console.log(slots[i-1]);
+        if(slots[i].classList.contains("green")){
+         // console.log('here');
+            emptyCounter +=1;
+        }
+     }
+//console.log(6-emptyCounter);
+    //slots[toCheck].classList.add("green");
+return 6-emptyCounter;
+}
 
