75fe9770ee324a1454cc7d911541be12eee2d3e6
neil314159 <neil@neilboland.com>
2021-11-25 19:54:19 +0000

    Remove whitespace from code and improve formatting

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,9 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-
     createBoard();
     createEventListeners();
-
 })
 
 function createEventListeners() {
@@ -16,24 +13,18 @@ function createEventListeners() {
     });
 
     var computerCheck = document.getElementById("computerPlayer");
-
     computerCheck.addEventListener("change", function () {
         clearBoard();
-
-
     });
 
     let buttons = document.getElementsByClassName("menu");
-
     buttons[0].addEventListener("click", function () {
-
         let faces = document.getElementsByClassName("box");
 
         if (faces[0].classList.contains("spinback")) {
             faces[0].classList.remove("spinback");
             aces[0].classList.add("spinfront");
         } else {
-
             faces[0].classList.remove("spinleft");
             faces[0].classList.remove("spinfront");
             faces[0].classList.add("spinback");
@@ -42,65 +33,37 @@ function createEventListeners() {
 
     let returnButtons = document.getElementsByClassName("returnHome");
     returnButtons[0].addEventListener("click", function () {
-
         let faces = document.getElementsByClassName("box");
         faces[0].classList.remove("spinleft");
         faces[0].classList.remove("spinback");
         faces[0].classList.add("spinfront");
-
     });
 
-    // returnButtons[1].addEventListener("click", function () {
-
-    //     let faces = document.getElementsByClassName("box");
-    //     faces[0].classList.remove("spinleft");
-    //     faces[0].classList.remove("spinback");
-    //     faces[0].classList.add("spinfront");
-
-    // });
-
     let buttons3 = document.getElementsByClassName("side");
-
     buttons3[0].addEventListener("click", function () {
-
         let faces = document.getElementsByClassName("box");
         faces[0].classList.add("spinleft");
-
     });
 
     let newGameButtons = document.getElementsByClassName("newGame");
-
     newGameButtons[0].addEventListener("click", function () {
-
         let faces = document.getElementsByClassName("box");
         faces[0].classList.remove("spinback");
         faces[0].classList.remove("spinleft");
         faces[0].classList.add("spinfront");
         clearBoard();
-
-
     });
 
     newGameButtons[1].addEventListener("click", function () {
-
         let faces = document.getElementsByClassName("box");
         faces[0].classList.remove("spinback");
         faces[0].classList.remove("spinleft");
         faces[0].classList.add("spinfront");
         clearBoard();
     });
-
-
-
-}
-
-function spinLeft() {
-    let faces = document.getElementsByClassName("box");
-    faces[0].classList.add("spinleft");
 }
 
 function createBoard() {
-
     for (let i = 0; i < 42; i++) {
         let slot = document.createElement('div');
         slot.setAttribute('data-id', i);
@@ -109,132 +72,71 @@ function createBoard() {
         slot.addEventListener("click", function () {
             gameClick(i);
         });
-
         document.getElementById('game').appendChild(slot);
     }
 }
 
-
 function clearBoard() {
-
     let slots = document.getElementsByClassName("slot");
     for (let slot of slots) {
 
         slot.classList.remove('clear', 'green', 'yellow');
         slot.classList.add('clear');
     };
-
     let playerColour = document.getElementById('player');
     playerColour.textContent = "yellow";
-
 }
 
 function gameClick(i) {
-
     let computerCheck = document.getElementById("computerPlayer");
     let playerColour = document.getElementById('player');
-
     let column = (i % 7);
     let empties = emptySpacesColumn(column);
     let newSlot = ((column + 1) + (7 * (empties - 1))) - 1;
     let slots = document.getElementsByClassName("slot");
 
-
-
     if (empties > 0) {
-
-        if (computerCheck.checked){
-
+        if (computerCheck.checked) {
             slots[newSlot].classList.add("yellow");
             checkForWin("yellow");
-
             let pcMove = computerNextMove();
-            
-       // console.log(pcMove);
-        let pcEmpty = emptySpacesColumn(pcMove);
-        //console.log(pcEmpty);
-        let pcSlot = ((pcMove + 1) + (7 * (pcEmpty - 1))) - 1;
-       // console.log(pcSlot);
-        slots[pcSlot].classList.add("green");
-        checkForWin("green");
-        playerColour.textContent = "yellow";
-
-        } else{
-
-        if (playerColour.textContent === "yellow") {
-
-            slots[newSlot].classList.add("yellow");
-            checkForWin("yellow");
-            playerColour.textContent = "green";
-        } else if (playerColour.textContent === "green") {
-            slots[newSlot].classList.add("green");
+            let pcEmpty = emptySpacesColumn(pcMove);
+            let pcSlot = ((pcMove + 1) + (7 * (pcEmpty - 1))) - 1;
+            slots[pcSlot].classList.add("green");
             checkForWin("green");
             playerColour.textContent = "yellow";
+        } else {
+            if (playerColour.textContent === "yellow") {
+                slots[newSlot].classList.add("yellow");
+                checkForWin("yellow");
+                playerColour.textContent = "green";
+            } else if (playerColour.textContent === "green") {
+                slots[newSlot].classList.add("green");
+                checkForWin("green");
+                playerColour.textContent = "yellow";
+            }
         }
+        checkForDraw();
     }
-    checkForDraw();
-}
-
-
-
-    // if (computerCheck.checked) {
-    //     //PC code
-    //     slots[newSlot].classList.remove("clear");
-    //     slots[newSlot].classList.add("yellow");
-    //     checkForWin("yellow");
-
-    //     //playerColour.textContent = "green";
-    //    // let p = computerNextMove();
-    //    // console.log('getting here');
-    //     let pcMove = computerNextMove();
-    //     console.log(pcMove);
-    //     let pcEmpty = emptySpacesColumn(pcMove);
-    //     let pcSlot = ((column + 1) + (7 * (pcEmpty - 1))) - 1;
-    //     console.log(pcSlot);
-    //     slots[pcSlot].classList.add("green");
-    //     checkForWin("green");
-    //     playerColour.textContent = "yellow";
-
-    // } else {
-
-
-       
-       
-
-    //     //computerNextMove();
-    //     //checkForDraw();
-
-    // }
-
-
-
 }
 
 function emptySpacesColumn(toCheck) {
-
     let slots = document.getElementsByClassName("slot");
     let emptyCounter = 0;
-
     for (let i = toCheck; i < 42; i += 7) {
-
         if (slots[i].classList.contains("green") || slots[i].classList.contains("yellow")) {
-
             emptyCounter += 1;
         }
     }
-
     return 6 - emptyCounter;
 }
 
 function checkForDraw() {
-
     let slots = document.getElementsByClassName("slot");
     let drawCounter = 0;
     for (let slot of slots) {
-
         if (slot.classList.contains("clear")) drawCounter++;
     }
-
     if (drawCounter === 0) {
         spinLeft();
     } else {
@@ -242,65 +144,36 @@ function checkForDraw() {
     }
 }
 
-
-
 function checkForWin(colourW) {
     let slots = document.getElementsByClassName("slot");
     let currentColour = colourW;
-   // console.log(currentColour);
-    //check horizontally
-
-
     for (let i = 0; i < 42; i += 7) {
         for (let k = 0; k < 4; k++) {
             if (slots[i + k].classList.contains(currentColour) && slots[i + k + 1].classList.contains(currentColour) && slots[i + k + 2].classList.contains(currentColour) && slots[i + k + 3].classList.contains(currentColour)) {
-                spinLeft();
+                showResults();
             }
         }
-
     }
-
-
     //check vertically
     for (let i = 0; i < 7; i++) {
-
         for (let k = 0; k < 3; k++) {
-
-            if (slots[i + (7 * k)].classList.contains(currentColour) && slots[i + (7 * k) + 7].classList.contains(currentColour) && slots[i + (7 * k) + 14].classList.contains(currentColour) && slots[i + (7 * k) + 21].classList.contains(currentColour)) spinLeft();
-
+            if (slots[i + (7 * k)].classList.contains(currentColour) && slots[i + (7 * k) + 7].classList.contains(currentColour) && slots[i + (7 * k) + 14].classList.contains(currentColour) && slots[i + (7 * k) + 21].classList.contains(currentColour)) showResults();
         }
     }
-
     //check diagonally top left to bottom right
     let leftRightStartingPoints = [0, 1, 2, 3, 7, 8, 9, 10, 14, 15, 16, 17];
-
     for (let i = 0; i < leftRightStartingPoints.length; i++) {
         let k = leftRightStartingPoints[i];
-
-        if (slots[k].classList.contains(currentColour) && slots[k + 8].classList.contains(currentColour) && slots[k + 16].classList.contains(currentColour) && slots[k + 24].classList.contains(currentColour)) spinLeft();
-
+        if (slots[k].classList.contains(currentColour) && slots[k + 8].classList.contains(currentColour) && slots[k + 16].classList.contains(currentColour) && slots[k + 24].classList.contains(currentColour)) showResults();
     }
-
-
     //check diagonally top right to bottom left
-
     let rightLeft = [3, 4, 5, 6, 13, 20];
     let rightLeftStartingPoints = [3, 4, 5, 6, 10, 11, 12, 13, 17, 18, 19, 20];
-
     for (let i = 0; i < rightLeftStartingPoints.length; i++) {
         let k = rightLeftStartingPoints[i];
-
-        if (slots[k].classList.contains(currentColour) && slots[k + 6].classList.contains(currentColour) && slots[k + 12].classList.contains(currentColour) && slots[k + 18].classList.contains(currentColour)) spinLeft();
-
+        if (slots[k].classList.contains(currentColour) && slots[k + 6].classList.contains(currentColour) && slots[k + 12].classList.contains(currentColour) && slots[k + 18].classList.contains(currentColour)) showResults();
     }
-
-
 }
-
-
-
-
-
 
 function computerNextMove() {
     let columnVotes = [0, 0, 0, 0, 0, 0, 0];
@@ -311,20 +184,16 @@ function computerNextMove() {
     //check base level first
     let baseCheckLeft = [38, 39, 40, 41];
     let baseCheckRight = [35, 36, 37, 38];
-
     for (let i = 38; i < 42; i++) {
         if (slots[i - 1].classList.contains(currentColour) && slots[i - 2].classList.contains(currentColour) && slots[i - 3].classList.contains(currentColour) && slots[i].classList.contains("clear")) {
             columnVotes[i % 7]++;
         }
     }
-
     for (let i = 35; i < 39; i++) {
         if (slots[i + 1].classList.contains(currentColour) && slots[i + 2].classList.contains(currentColour) && slots[i + 3].classList.contains(currentColour) && slots[i].classList.contains("clear")) {
             columnVotes[i % 7]++;
         }
     }
-    //console.log(columnVotes);
-
     //check rows above base row
     for (let k = 0; k < 29; k += 7) {
         for (let i = 3; i < 7; i++) {
@@ -334,7 +203,6 @@ function computerNextMove() {
             }
         }
     }
-
     for (let k = 0; k < 29; k += 7) {
         for (let i = 0; i < 4; i++) {
             let m = i + k;
@@ -345,56 +213,49 @@ function computerNextMove() {
     }
 
     //check vertical tokens
-
-
     for (let i = 0; i < 7; i++) {
-
         for (let k = 0; k < 3; k++) {
-
             if (slots[i + (7 * k)].classList.contains("clear") && slots[i + (7 * k) + 7].classList.contains(currentColour) && slots[i + (7 * k) + 14].classList.contains(currentColour) && slots[i + (7 * k) + 21].classList.contains(currentColour)) columnVotes[i % 7]++;
-
         }
     }
     //check for diagonal winnnig streaks
-
     let leftRightStartingPoints = [0, 1, 2, 3, 7, 8, 9, 10, 14, 15, 16, 17];
-
     for (let i = 0; i < leftRightStartingPoints.length; i++) {
         let k = leftRightStartingPoints[i];
-
         if (slots[k].classList.contains("clear") && (slots[k + 7].classList.contains("green") || slots[k + 7].classList.contains("yellow")) && slots[k + 8].classList.contains(currentColour) && slots[k + 16].classList.contains(currentColour) && slots[k + 24].classList.contains(currentColour)) columnVotes[k % 7]++;
-
     }
-    //console.log(columnVotes);
-
     //check diagonally top right to bottom left
-
     let rightLeft = [3, 4, 5, 6, 13, 20];
     let rightLeftStartingPoints = [3, 4, 5, 6, 10, 11, 12, 13, 17, 18, 19, 20];
-
     for (let i = 0; i < rightLeftStartingPoints.length; i++) {
         let k = rightLeftStartingPoints[i];
-
         if (slots[k].classList.contains("clear") && (slots[k + 7].classList.contains("green") || slots[k + 7].classList.contains("yellow")) && slots[k + 6].classList.contains(currentColour) && slots[k + 12].classList.contains(currentColour) && slots[k + 18].classList.contains(currentColour)) columnVotes[k % 7]++;
-
     }
-    //console.log(columnVotes);
-
-    //console.log(newI);
     if (Math.max(...columnVotes) === 0) {
         nextMove = Math.floor(Math.random() * 6);
     } else {
         nextMove = columnVotes.indexOf(Math.max(...columnVotes));
     }
-if(emptySpacesColumn(nextMove)===0){
-
-
-for(let i=0; i<7;i++){
-if(emptySpacesColumn(i)>0) nextMove = i;
-}
-}
-    //emptySpacesColumn(column);
-
+    if (emptySpacesColumn(nextMove) === 0) {
+        for (let i = 0; i < 7; i++) {
+            if (emptySpacesColumn(i) > 0) nextMove = i;
+        }
+    }
     return nextMove;
+}
 
+function showResults() {
+    let faces = document.getElementsByClassName("box");
+    faces[0].classList.add("spinleft");
+    let computerCheck = document.getElementById("computerPlayer");
+    let playerColour = document.getElementById('player');
+    let resultsText = document.getElementById('resultstext');
+
+    if (computerCheck.checked) {
+
+        if (playerColour.textContent === "green") resultsText.innerText = 'computer won';
+        else resultsText.innerText = 'you won';
+    } else {
+        resultsText.innerText = `${playerColour.textContent} won`;
+    }
 }
