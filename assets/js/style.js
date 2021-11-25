cc0a71844761307dcf3a78535317db413630abed
neil314159 <neil@neilboland.com>
2021-11-25 00:32:53 +0000

    Add inital game logic in javascript

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,13 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(){
-    let slots = document.getElementsByClassName("slot");
-
-
-    for(let slot of slots){
-        slot.addEventListener("click", function(){
-        this.classList.add("green");
-    });
-    }
-
+    
+    
+//    console.log("first")
+createBoard();
+    
+//  let slots = document.getElementsByClassName("slot");
+//     for(let slot of slots){
+//         slot.addEventListener("click", function(){
+//         this.classList.add("green");
+//     });
+//     }
 
     let buttons = document.getElementsByClassName("menu");
     // console.log(buttons);
@@ -15,9 +17,9 @@ document.addEventListener("DOMContentLoaded", function(){
 
         let faces = document.getElementsByClassName("box");
         faces[0].classList.add("spinback");
-        let shadows = document.getElementsByClassName("shadow");
-        console.log(shadows);
-        shadows[0].classList.add("spinbackshadow");
+        // let shadows = document.getElementsByClassName("shadow");
+        // console.log(shadows);
+        // shadows[0].classList.add("spinbackshadow");
     });
 
     let buttons2 = document.getElementsByClassName("front");
@@ -28,14 +30,47 @@ document.addEventListener("DOMContentLoaded", function(){
         let faces = document.getElementsByClassName("box");
         faces[0].classList.remove("spinback");
         faces[0].classList.add("spinfront");
+clearBoard();
 
-
-        let shadows = document.getElementsByClassName("shadow");
-        console.log(shadows);
-        shadows[0].classList.remove("spinbackshadow");
-        shadows[0].classList.add("spinfrontshadow");
+        // let shadows = document.getElementsByClassName("shadow");
+        // console.log(shadows);
+        // shadows[0].classList.remove("spinbackshadow");
+        // shadows[0].classList.add("spinfrontshadow");
     });
     
 })
+
+function createBoard(){
+
+    for(let i=0;i<42; i++){
+        let slot = document.createElement('div');
+         slot.setAttribute('data-id', i);
+        slot.classList.add('slot');
+        slot.classList.add('clear');
+        slot.addEventListener("click", function(){
+            //console.log(this);
+            this.classList.add("green");
+        });
+        document.getElementById('game').appendChild(slot);
+    }
+}
+
+
+function clearBoard(){
+
+ let slots = document.getElementsByClassName("slot");
+    for(let slot of slots){
+       // console.log(this.classList);
+        slot.classList.remove('clear', 'green');
+        slot.classList.add('clear');
+    };
+    
+}
+
+function gameClick(){
+   
+       
+    
+}
 
 
