e267126e61c8a64e222a49d726a357f01bfedf0f
neil314159 <neil@neilboland.com>
2021-11-26 04:26:58 +0000

    Fix logic bug in main game function

--- assets/js/style.js
+++ assets/js/style.js
@@ -5,23 +5,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
 /**Generates and attaches all event listeners for buttons and checkboxed */
 function createEventListeners() {
+    // dark mode status checkbox
     var darkcheckbox = document.getElementById("darkmodecheck");
-
     darkcheckbox.addEventListener("change", function () {
         var element = document.body;
         element.classList.toggle("darkmode");
-
     });
 
+    // human or computer opponent check 
     var computerCheck = document.getElementById("computerPlayer");
     computerCheck.addEventListener("change", function () {
         clearBoard();
     });
 
-    let buttons = document.getElementsByClassName("menu");
-    buttons[0].addEventListener("click", function () {
+    // manage CSS classes to get to menu option 
+    let menuButtons = document.getElementsByClassName("menu");
+    menuButtons[0].addEventListener("click", function () {
         let faces = document.getElementsByClassName("box");
-
         if (faces[0].classList.contains("spinback")) {
             faces[0].classList.remove("spinback");
             faces[0].classList.add("spinfront");
@@ -32,6 +32,7 @@ function createEventListeners() {
         }
     });
 
+    // return to game being played without resetting it 
     let returnButtons = document.getElementsByClassName("returnHome");
     returnButtons[0].addEventListener("click", function () {
         let faces = document.getElementsByClassName("box");
@@ -40,14 +41,12 @@ function createEventListeners() {
         faces[0].classList.add("spinfront");
     });
 
-
-
+    // start a new game 
     let newGameButtons = document.getElementsByClassName("newGame");
     newGameButtons[0].addEventListener("click", function () {
         let faces = document.getElementsByClassName("box");
         faces[0].classList.remove("spinback");
         faces[0].classList.remove("spinleft");
-        //faces[0].classList.add("spinright");
         faces[0].classList.add("spinfront");
         clearBoard();
     });
@@ -56,18 +55,16 @@ function createEventListeners() {
         let faces = document.getElementsByClassName("box");
         faces[0].classList.remove("spinback");
         faces[0].classList.remove("spinleft");
-        //faces[0].classList.add("spinright");
         faces[0].classList.add("spinfront");
         clearBoard();
     });
 }
+
 /**Sets up board at beginning of game */
 function createBoard() {
     for (let i = 0; i < 42; i++) {
         let slot = document.createElement('div');
         slot.setAttribute('data-id', i);
-        // slot.tabIndex = 0;
-        //  slot.onkeydown = function(){this.click();};
         slot.classList.add('slot');
         slot.classList.add('clear');
         slot.addEventListener("click", function () {
@@ -81,9 +78,6 @@ function createBoard() {
 function clearBoard() {
     let slots = document.getElementsByClassName("slot");
     for (let slot of slots) {
-
-        // slot.classList.remove('clear', 'green', 'yellow');
-        // slot.classList.add('green');
         slot.classList.remove('clear', 'green', 'yellow');
         slot.classList.add('clear');
     }
@@ -107,27 +101,33 @@ function gameClick(i) {
 
     if (empties > 0) {
         if (computerCheck.checked) {
+            slots[newSlot].classList.remove("clear");
             slots[newSlot].classList.add("yellow");
             checkForWin("yellow");
             let pcMove = computerNextMove();
             let pcEmpty = emptySpacesColumn(pcMove);
             let pcSlot = ((pcMove + 1) + (7 * (pcEmpty - 1))) - 1;
+            slots[pcSlot].classList.remove("clear");
             slots[pcSlot].classList.add("green");
+            
+            playerColour.textContent = "green";
             checkForWin("green");
-            playerColour.textContent = "yellow";
+           playerColour.textContent = "yellow";
         } else {
             if (playerColour.textContent === "yellow") {
+                slots[newSlot].classList.remove("clear");
                 slots[newSlot].classList.add("yellow");
                 checkForWin("yellow");
                 playerColour.textContent = "green";
             } else if (playerColour.textContent === "green") {
+                slots[newSlot].classList.remove("clear");
                 slots[newSlot].classList.add("green");
                 checkForWin("green");
                 playerColour.textContent = "yellow";
             }
         }
-        checkForDraw();
     }
+    checkForDraw();
 }
 
 /**This returns the number of empty spaces available for a token in the given column */
@@ -142,7 +142,7 @@ function emptySpacesColumn(toCheck) {
     return 6 - emptyCounter;
 }
 
-/**This function evaluates the board to see if a fraw has been reached */
+/**This function evaluates the board to see if a draw has been reached */
 function checkForDraw() {
     let slots = document.getElementsByClassName("slot");
     let drawCounter = 0;
@@ -150,10 +150,8 @@ function checkForDraw() {
         if (slot.classList.contains("clear")) drawCounter++;
     }
     if (drawCounter === 0) {
-        //draw condition showResults();
-    } else {
-
-    }
+         showResults(true);
+    } 
 }
 
 /**This function iterates through the entire board and checks for a pattern of four pieces in a line
@@ -162,30 +160,32 @@ function checkForDraw() {
 function checkForWin(colourW) {
     let slots = document.getElementsByClassName("slot");
     let currentColour = colourW;
+    
+    // check horizontally 
     for (let i = 0; i < 42; i += 7) {
         for (let k = 0; k < 4; k++) {
             if (slots[i + k].classList.contains(currentColour) && slots[i + k + 1].classList.contains(currentColour) && slots[i + k + 2].classList.contains(currentColour) && slots[i + k + 3].classList.contains(currentColour)) {
-                return showResults();
+                showResults(false);
             }
         }
     }
-    //check vertically
+    // check vertically
     for (let i = 0; i < 7; i++) {
         for (let k = 0; k < 3; k++) {
-            if (slots[i + (7 * k)].classList.contains(currentColour) && slots[i + (7 * k) + 7].classList.contains(currentColour) && slots[i + (7 * k) + 14].classList.contains(currentColour) && slots[i + (7 * k) + 21].classList.contains(currentColour)) return showResults();
+            if (slots[i + (7 * k)].classList.contains(currentColour) && slots[i + (7 * k) + 7].classList.contains(currentColour) && slots[i + (7 * k) + 14].classList.contains(currentColour) && slots[i + (7 * k) + 21].classList.contains(currentColour)) return showResults(false);
         }
     }
     //check diagonally top left to bottom right
     let leftRightStartingPoints = [0, 1, 2, 3, 7, 8, 9, 10, 14, 15, 16, 17];
     for (let i = 0; i < leftRightStartingPoints.length; i++) {
         let k = leftRightStartingPoints[i];
-        if (slots[k].classList.contains(currentColour) && slots[k + 8].classList.contains(currentColour) && slots[k + 16].classList.contains(currentColour) && slots[k + 24].classList.contains(currentColour)) return showResults();
+        if (slots[k].classList.contains(currentColour) && slots[k + 8].classList.contains(currentColour) && slots[k + 16].classList.contains(currentColour) && slots[k + 24].classList.contains(currentColour)) return showResults(false);
     }
     //check diagonally top right to bottom left
     let rightLeftStartingPoints = [3, 4, 5, 6, 10, 11, 12, 13, 17, 18, 19, 20];
     for (let i = 0; i < rightLeftStartingPoints.length; i++) {
         let k = rightLeftStartingPoints[i];
-        if (slots[k].classList.contains(currentColour) && slots[k + 6].classList.contains(currentColour) && slots[k + 12].classList.contains(currentColour) && slots[k + 18].classList.contains(currentColour)) return showResults();
+        if (slots[k].classList.contains(currentColour) && slots[k + 6].classList.contains(currentColour) && slots[k + 12].classList.contains(currentColour) && slots[k + 18].classList.contains(currentColour)) return showResults(false);
     }
 }
 
@@ -197,10 +197,8 @@ function computerNextMove() {
     let columnVotes = [0, 0, 0, 0, 0, 0, 0];
     let slots = document.getElementsByClassName("slot");
     let currentColour = "green";
-    let nextMove = 3;
-
+    let nextMove = 3; //default to center column move
     //check base level first
-
     for (let i = 38; i < 42; i++) {
         if (slots[i - 1].classList.contains(currentColour) && slots[i - 2].classList.contains(currentColour) && slots[i - 3].classList.contains(currentColour) && slots[i].classList.contains("clear")) {
             columnVotes[i % 7]++;
@@ -228,7 +226,6 @@ function computerNextMove() {
             }
         }
     }
-
     //check vertical tokens
     for (let i = 0; i < 7; i++) {
         for (let k = 0; k < 3; k++) {
@@ -242,7 +239,6 @@ function computerNextMove() {
         if (slots[k].classList.contains("clear") && (slots[k + 7].classList.contains("green") || slots[k + 7].classList.contains("yellow")) && slots[k + 8].classList.contains(currentColour) && slots[k + 16].classList.contains(currentColour) && slots[k + 24].classList.contains(currentColour)) columnVotes[k % 7]++;
     }
     //check diagonally top right to bottom left
-
     let rightLeftStartingPoints = [3, 4, 5, 6, 10, 11, 12, 13, 17, 18, 19, 20];
     for (let i = 0; i < rightLeftStartingPoints.length; i++) {
         let k = rightLeftStartingPoints[i];
@@ -262,19 +258,20 @@ function computerNextMove() {
 }
 
 /**Display the results of the game to the user */
-function showResults() {
+function showResults(draw) {
     let faces = document.getElementsByClassName("box");
     faces[0].classList.add("spinleft");
     let computerCheck = document.getElementById("computerPlayer");
     let playerColour = document.getElementById('player');
     let resultsText = document.getElementById('resultstext');
-
+    if(draw){
+        resultsText.innerText = "It's a draw!";
+        return true;
+    }
     if (computerCheck.checked) {
-        console.log(playerColour.textContent);
         if (playerColour.textContent === "green") resultsText.innerText = 'The computer won!';
         else resultsText.innerText = 'You won!';
     } else {
         resultsText.innerText = `${playerColour.textContent} won!`;
-
     }
 }
